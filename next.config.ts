7f89298512bf62af272240fbd5697244df5b31e7
patelbharat001 <patelbharat001@gmail.com>
2026-08-25 00:20:13 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubActions ? "/ziporter" : "",
-  assetPrefix: isGithubActions ? "/ziporter" : "",
+  basePath: "/ziporter",
+  assetPrefix: "/ziporter",
   trailingSlash: true,
   images: { unoptimized: true },
 };
